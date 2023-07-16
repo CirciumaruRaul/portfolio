f782b1ea33a65cab348e527d963ceecb57ce6619
CirciumaruRaul <119003892+CirciumaruRaul@users.noreply.github.com>
2023-07-16 12:45:11 +0300

    Update Skill.tsx
    Bug fix for small devices where hover effect is bigger then the image

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -16,9 +16,9 @@ function Skill(props: Props) {
         src={props.poza}  //aici trb niste poza
         alt={'eu'} 
         className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out max-[500px]:h-14 max-[500px]:w-14'/>
-        <div className='absolute opacity-0 group-hover:opacity-80 tranzition duration-300 ease-in-out group-hover:bg-white h-24 w-24 xl:w-32 xl:h-32 rounded-full z-0 '>
-            <div className='flex items-center justify-center h-full'>
-                <p className='text-3xl font-bold text-black opacity-100'>{props.procent}%</p>
+        <div className='absolute opacity-0 group-hover:opacity-80 tranzition duration-300 ease-in-out group-hover:bg-white h-24 w-24 xl:w-32 xl:h-32 rounded-full z-0 max-[500px]:h-14 max-[500px]:w-14'>
+            <div className='flex items-center justify-center h-full max-[500px]:h-14 max-[500px]:w-14'>
+                <p className='text-3xl font-bold text-black opacity-100 max-[500px]:text-base'>{props.procent}%</p>
             </div>
         </div>
     </div>
